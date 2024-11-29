a78930309997e67f64a3fc4e889ea21af58c77be
João Pedro <jonhpitter2004@hotmail.com>
2024-11-29 19:56:15 -0300

    first commit

---
+++ src/main/java/com/mycompany/smartcondos/SmartCondos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.smartcondos;
+
+/**
+ *
+ * @author joaopedro
+ */
+public class SmartCondos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
